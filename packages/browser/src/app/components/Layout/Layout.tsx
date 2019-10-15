767c1a218fc88cf406db62a2a4abd555848506b9
Aleksander Fret <alemik@op.pl>
2019-10-15 20:44:40 +0200

    [PE-2] Init React

---
+++ packages/browser/src/app/components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+import classes from './Layout.scss';
+
+const Layout: FC = () => (
+  <div className={classes.layout}>
+    <h1>Plan Event App</h1>
+  </div>
+);
+
+export default Layout;
---
+++ packages/browser/src/app/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Layout from './components/Layout/Layout';
+import './scss/index.scss';
+
+ReactDOM.render(<Layout />, document.getElementById('root'));
