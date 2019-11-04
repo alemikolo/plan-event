0396f80e3dc2fe8d007da7bd88b8f986fd98cd86
Aleksander Fret <alemik@op.pl>
2019-11-04 23:06:49 +0100

    [PE-3] Refactor module structure

--- packages/server/src/modules/event/entities.ts
+++ packages/server/src/modules/event/entities.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-import { EventStatus, EventVisibility } from '../enums';
+import { EventStatus, EventVisibility } from './enums';
 
 @Entity()
 export class Event {
@@ -10,32 +10,25 @@ export class Event {
   @Column({ nullable: true })
   deleteDate: Date;
 
-  @Column({
-    length: 1000,
-    nullable: true,
-    type: 'varchar'
-  })
+  @Column({ length: 1000, nullable: true, type: 'varchar' })
   description: string;
 
   @Column({ nullable: true })
   endDate: Date;
 
-  @Column({
-    length: 50,
-    nullable: false,
-    type: 'varchar'
-  })
+  @Column({ nullable: true, precision: 8, scale: 6, type: 'decimal' })
+  latitude: number;
+
+  @Column({ nullable: true, precision: 9, scale: 6, type: 'decimal' })
+  longitude: number;
+
+  @Column({ length: 50, nullable: false, type: 'varchar' })
   name: string;
 
   @Column({ nullable: false })
   organizer: number;
 
-  @Column({
-    nullable: true,
-    precision: 10,
-    scale: 2,
-    type: 'decimal'
-  })
+  @Column({ nullable: true, precision: 10, scale: 2, type: 'decimal' })
   price: number;
 
   @Column({ nullable: true })
--- packages/server/src/modules/event/routes.ts
+++ packages/server/src/modules/event/routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 
-import { applyRoutes } from '../../../common/helpers';
-import { Crud } from '../../../common/enums';
-import { getEvents } from '../controllers';
+import { applyRoutes } from '../../common/helpers';
+import { Crud } from '../../common/enums';
+import { getEvents } from './controllers';
 
 export default applyRoutes(Router())('/api/event')([
   { method: Crud.get, path: '/get', handlers: [getEvents] }
